979b70f941cb6c28910920dc86859fd163cc3d52
Thog <drbitey@gmail.com>
2020-12-09 17:21:50 -0600

    christmas
    sets countdown date to christmas

--- countdown.js
+++ countdown.js
@@ -1,14 +1,17 @@
-var countDownDate = new Date("June 20, 2076 00:00:00").getTime();
+var countDownDate = new Date("December 25, 2020 00:00:00").getTime();
 //const text = document.getElementById("countdown").innerHTML;
 var interval = 11; //time between checking time in ms
-var x = setInterval(function() {
-	var now = new Date().getTime();
-	var distance = countDownDate - now;
-	if ((distance / (1000 * 60 * 60 * 24 * 365)) < 0) {var years = Math.ceil(distance / (1000 * 60 * 60 * 24 * 365))}
-	else {var years = Math.floor(distance / (1000 * 60 * 60 * 24 * 365))}
-	var days = Math.floor(distance % (1000 * 60 * 60 * 24 * 365) / (1000 * 60 * 60 * 24));
-	var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-	var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-	var seconds = ((distance % (1000 * 60)) / 1000).toFixed(3);
-	document.getElementById("countdown").innerHTML = years + "y " + days + "d " + hours + "h " + minutes + "m " + seconds + "s";
-}, interval);
+if ((countDownDate - new Date().getTime()) < 0) {} //do nothing if date has passed
+else{
+	var x = setInterval(function() {
+		var now = new Date().getTime();
+		var distance = countDownDate - now;
+		if ((distance / (1000 * 60 * 60 * 24 * 365)) < 0) {var years = Math.ceil(distance / (1000 * 60 * 60 * 24 * 365))}
+		else {var years = Math.floor(distance / (1000 * 60 * 60 * 24 * 365))}
+		var days = Math.floor(distance % (1000 * 60 * 60 * 24 * 365) / (1000 * 60 * 60 * 24));
+		var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+		var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+		var seconds = ((distance % (1000 * 60)) / 1000).toFixed(3);
+		document.getElementById("countdown").innerHTML = years + "y " + days + "d " + hours + "h " + minutes + "m " + seconds + "s";
+	}, interval);
+}
