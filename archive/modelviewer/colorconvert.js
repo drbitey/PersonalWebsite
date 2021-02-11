2cb4d6c67c2990db2926fad8d012c283f4988e4c
Thog <drbitey@gmail.com>
2021-02-11 14:21:48 -0600

    wireframe 8

---
+++ archive/modelviewer/colorconvert.js
@@ -0,0 +1,41 @@
+export function randomHex(h, s, v) {
+	s /= 100;
+	v /= 100;
+
+	let c = (1 - Math.abs(2 * v - 1)) * s,
+	x = c * (1 - Math.abs((h / 60) % 2 - 1)),
+	m = v - c/2,
+	r = 0,
+	g = 0, 
+	b = 0; 
+	
+	//convert hsv to rgb
+	if (0 <= h && h < 60) {
+		r = c; g = x; b = 0;
+	} else if (60 <= h && h < 120) {
+		r = x; g = c; b = 0;
+	} else if (120 <= h && h < 180) {
+		r = 0; g = c; b = x;
+	} else if (180 <= h && h < 240) {
+		r = 0; g = x; b = c;
+	} else if (240 <= h && h < 300) {
+		r = x; g = 0; b = c;
+	} else if (300 <= h && h < 360) {
+		r = c; g = 0; b = x;
+	}
+	
+	//convert rgb channels to hex
+	r = Math.round((r + m) * 255).toString(16);
+	g = Math.round((g + m) * 255).toString(16);
+	b = Math.round((b + m) * 255).toString(16);
+
+	//prepending necessary 0s
+	if (r.length == 1)
+		r = "0" + r;
+	if (g.length == 1)
+		g = "0" + g;
+	if (b.length == 1)
+		b = "0" + b;
+
+	return "#" + r + g + b;
+}
--- archive/modelviewer/main.js
+++ archive/modelviewer/main.js
@@ -1,11 +1,12 @@
+//js refs
 import {square, doubleSquare, cube, pyramid} from './models.js';
 import {Camera} from './camera.js';
 import {createMesh} from './mesh.js';
 import {createWireframeRenderer} from './render.js';
-
-const canvas = document.querySelector('canvas');
+import {randomHex} from './colorconvert.js';
 
 //html refs
+const canvas = document.querySelector('canvas');
 var x = document.getElementById("xrot");
 var y = document.getElementById("yrot");
 var wfcol = document.getElementById("wfcol");
@@ -14,13 +15,15 @@ var outx = document.getElementById("testx");
 var outy = document.getElementById("testy");
 
 const mesh1 = createMesh(cube);
-mesh1.color = '#81ff02';
+mesh1.color = '#81ff02'; //temp
 /*const mesh2 = createMesh(pyramid);
 mesh2.color = '#ff02f0';
 const mesh3 = createMesh(cube);
 mesh3.color = '#ff026d';*/
 
-const scene = [mesh1, /*mesh2, mesh3*/];
+//separate meshes to be fixed up later, not important now
+
+const scene = [mesh1, /*mesh2, mesh3*/]; //^^
 
 const camera = new Camera();
 camera.pos.z = 200;
@@ -28,13 +31,19 @@ camera.zoom = 12;
 
 const render = createWireframeRenderer(canvas);
 
-//init stuff
+//init stuff, not really necessary to be in a function but nice housekeeping
 
 function init() {
+	//set rot value
 	mesh1.rotation.x = 0;
 	mesh1.rotation.y = 0;
 	outx.innerHTML = x.value;
 	outy.innerHTML = y.value;
+	
+	//random hex for wf col, from hsv for better value control
+	wfcolINIT = randomHex((math.floor(360 * math.Random())), 100%, 100%);
+	mesh1.color = wfcolINIT;
+	wfcol.value = wfcolINIT;
 	
 	render(scene, camera);
 }
