e861e21ef5a2db346fc020309b495cfb753d517a
Thog <drbitey@gmail.com>
2021-02-10 23:06:27 -0600

    wireframe 2

--- archive/modelviewer/main.js
+++ archive/modelviewer/main.js
@@ -7,12 +7,12 @@ const canvas = document.querySelector('canvas');
 
 const mesh1 = createMesh(pyramid);
 mesh1.color = '#81ff02';
-const mesh2 = createMesh(cube);
+/*const mesh2 = createMesh(cube);
 mesh2.color = '#ff02f0';
 const mesh3 = createMesh(cube);
-mesh3.color = '#ff026d';
+mesh3.color = '#ff026d';*/
 
-const scene = [mesh1, mesh2, mesh3];
+const scene = [mesh1, /*mesh2, mesh3*/];
 
 const camera = new Camera();
 camera.pos.z = 200;
@@ -21,6 +21,7 @@ camera.zoom = 12;
 const render = createWireframeRenderer(canvas);
 
 function animate(time) {
+	/*
     mesh1.position.x = Math.sin(time / 1000) * 100;
     mesh1.position.z = Math.sin(time / 1200) * 100;
     mesh1.rotation.x += 0.01;
@@ -34,7 +35,9 @@ function animate(time) {
     mesh3.position.x = Math.sin(time / 500) * 100;
     mesh3.position.y = Math.cos(time / 500) * 100;
     mesh3.rotation.y -= 0.005;
-
+	*/
+	mesh1.rotation.x += event.clientX / 100;
+	mesh1.rotation.y += event.clientY / 100;
     render(scene, camera);
 
     requestAnimationFrame(animate);
