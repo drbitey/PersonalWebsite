19a1d91391e585df81925dee0cbf64d4c774aa4f
Thog <drbitey@gmail.com>
2021-03-03 23:01:11 -0600

    Update cd2.js

--- cd2.js
+++ cd2.js
@@ -4,11 +4,9 @@ var interval = 100; //time between checking time in ms
 var x = setInterval(function() {
 	var now = new Date().getTime();
 	var distance = countDownDate - now;
-	if ((distance / (1000 * 60 * 60 * 24 * 365)) < 0) {var years = Math.ceil(distance / (1000 * 60 * 60 * 24 * 365))}
-	else {var years = Math.floor(distance / (1000 * 60 * 60 * 24 * 365))}
 	var days = Math.floor(distance % (1000 * 60 * 60 * 24 * 365) / (1000 * 60 * 60 * 24));
 	var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
 	var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
 	var seconds = ((distance % (1000 * 60)) / 1000).toFixed(0);
-	document.getElementById("countdown").innerHTML = years + "y " + days + "d " + hours + "h " + minutes + "m " + seconds + "s";
+	document.getElementById("countdown").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s";
 }, interval);
