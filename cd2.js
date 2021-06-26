61ce6b389aaddfbfa353ab0749374aaf6fe42faa
Thog <drbitey@gmail.com>
2021-06-25 22:28:34 -0500

    Update cd2.js

--- cd2.js
+++ cd2.js
@@ -9,5 +9,5 @@ var x = setInterval(function() {
 	var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
 	var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
 	var seconds = ((distance % (1000 * 60)) / 1000).toFixed(0);
-	document.getElementById("countdown").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s";
+	document.getElementById("countdown").innerHTML = years + "y " + days + "d " + hours + "h " + minutes + "m " + seconds + "s";
 }, interval);
