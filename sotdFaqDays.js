aa95834200f7cabc634f76a019321fd438289822
drbitey <drbitey@gmail.com>
2023-10-25 11:48:49 -0500

    Update sotdFaqDays.js

--- sotdFaqDays.js
+++ sotdFaqDays.js
@@ -1,5 +1,5 @@
 var startDate = new Date("February 5, 2022 00:00:00").getTime();
 var now = new Date().getTime();
 var distance = startDate - now;
-var days = Math.abs(Math.ceil(Math.floor(distance % (1000 * 60 * 60 * 24 * 365) / (1000 * 60 * 60 * 24))));
+var days = Math.abs(Math.ceil(Math.floor(distance / (1000 * 60 * 60 * 24))));
 document.getElementById("h4_gray_cd").innerHTML = "As of today, there is a total of " + days + " entries since February 5, 2022.";
