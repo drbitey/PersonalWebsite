8f3d51325a0253a6276f4fc810bbaeb4354afc0f
drbitey <drbitey@gmail.com>
2023-11-14 17:57:35 -0600

    Update loadSongData.js

--- sotd/loadSongData.js
+++ sotd/loadSongData.js
@@ -1,21 +1,24 @@
-function formatDateFromISO(isoDate) {
+function formatDateFromISO(isoDate, language) {
     const parts = isoDate.split('-'); // Split the ISO date into parts
     const year = parts[0];
     const month = parseInt(parts[1], 10);
     const day = parseInt(parts[2], 10);
 
     // Array of month names. Starts at 0, hence 'x'
-    const monthNames = [
-        'x', 'January', 'February', 'March', 'April', 'May', 'June',
-        'July', 'August', 'September', 'October', 'November', 'December'
-    ];
+    const monthNames = {
+        'en': ['x', 'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+        'de': ['x', 'Januar', 'Februar', 'März', 'April', 'Mai', 'Juni', 'Juli', 'August', 'September', 'Oktober', 'November', 'Dezember'],
+        // Add more languages as needed
+    };
+
+    const monthName = monthNames[language][monthIndex];
 
     // Format the date as "Month Day, Year"
-    const formattedDate = `${monthNames[month]} ${day}, ${year}`;
+    const formattedDate = `${monthName} ${day}, ${year}`;
     return formattedDate;
 }
 
-async function loadSongData() {
+async function loadSongData(language) {
     const response = await fetch('./sotd/sotdEntries.json');
     const data = await response.json();
 
@@ -24,7 +27,7 @@ async function loadSongData() {
     data.forEach((song) => {
         const songEntry = document.createElement('h4');
         const link = document.createElement('a');
-        const formattedDate = formatDateFromISO(song.date);
+        const formattedDate = formatDateFromISO(song.date, language);
         link.href = song.spotifyLink;
         link.textContent = `${formattedDate}: ${song.artist} - ${song.song_title}`;
         songEntry.appendChild(link);
