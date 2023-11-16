7daeb1de28a62b75793396f2c1bfc6aa10a36302
drbitey <drbitey@gmail.com>
2023-11-15 21:03:00 -0600

    Update loadSongData.js

--- sotd/loadSongData.js
+++ sotd/loadSongData.js
@@ -6,6 +6,38 @@ function convertToArabicNumerals(number) {
         .join('');
 }
 
+function convertToRomanNumerals(num) {
+  if (num <= 0 || num > 3999) {
+    return "Too high";
+  }
+  const romanNumerals = [
+    { value: 1000, numeral: "M" },
+    { value: 900, numeral: "CM" },
+    { value: 500, numeral: "D" },
+    { value: 400, numeral: "CD" },
+    { value: 100, numeral: "C" },
+    { value: 90, numeral: "XC" },
+    { value: 50, numeral: "L" },
+    { value: 40, numeral: "XL" },
+    { value: 10, numeral: "X" },
+    { value: 9, numeral: "IX" },
+    { value: 5, numeral: "V" },
+    { value: 4, numeral: "IV" },
+    { value: 1, numeral: "I" }
+  ];
+  let result = "";
+  for (let i = 0; i < romanNumerals.length; i++) {
+    while (num >= romanNumerals[i].value) {
+      result += romanNumerals[i].numeral;
+      num -= romanNumerals[i].value;
+    }
+  }
+  return result;
+}
+
+// Example usage:
+console.log(convertToRoman(63)); // Output: LXIII
+
 function formatDateFromISO(isoDate, language) {
     const parts = isoDate.split('-'); // Split the ISO date into parts
     const year = parts[0];
@@ -14,12 +46,13 @@ function formatDateFromISO(isoDate, language) {
 
     // Array of month names. Starts at 0, hence 'x'
     const monthNames = {
-        'ar': ['خ', 'يناير', 'فبراير', 'مارس', 'إبريل', 'مايو', 'يونيو', 'يوليو', 'أغسطس', 'سبتمبر', 'أكتوبر', 'نوفمبر', 'ديسمب'],
-        'br': ['x', 'Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'],
-        'de': ['x', 'Januar', 'Februar', 'März', 'April', 'Mai', 'Juni', 'Juli', 'August', 'September', 'Oktober', 'November', 'Dezember'],
-        'en': ['x', 'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
-        'it': ['x', 'Gennaio', 'Febbraio', 'Marzo', 'Aprile', 'Maggio', 'Giugno', 'Luglio', 'Agosto', 'Settembre', 'Ottobre', 'Novembre', 'Dicembre'],
-        'nl': ['x', 'Januari', 'Februari', 'Maart', 'April', 'Mei', 'Juni', 'Juli', 'Augustus', 'September', 'Oktober', 'November', 'December']
+        'ang': ['x', '', '', '', '', '', '', '', '', '', '', '', ''], //Ænglisċ/Old English
+        'ar': ['خ', 'يناير', 'فبراير', 'مارس', 'إبريل', 'مايو', 'يونيو', 'يوليو', 'أغسطس', 'سبتمبر', 'أكتوبر', 'نوفمبر', 'ديسمب'], //اَلْعَرَبِيَّةُ/Arabic
+        'br': ['x', 'Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'], //Portuguese do Brasil/Portuguese (br)
+        'de': ['x', 'Januar', 'Februar', 'März', 'April', 'Mai', 'Juni', 'Juli', 'August', 'September', 'Oktober', 'November', 'Dezember'],//Deutsch/German
+        'en': ['x', 'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],//English
+        'it': ['x', 'Gennaio', 'Febbraio', 'Marzo', 'Aprile', 'Maggio', 'Giugno', 'Luglio', 'Agosto', 'Settembre', 'Ottobre', 'Novembre', 'Dicembre'],//Italiano/Italian
+        'nl': ['x', 'Januari', 'Februari', 'Maart', 'April', 'Mei', 'Juni', 'Juli', 'Augustus', 'September', 'Oktober', 'November', 'December']//Nederlands/Dutch
         // languages to add
     };
 
@@ -32,6 +65,8 @@ function formatDateFromISO(isoDate, language) {
     let formattedDate;
 
     switch (language) {
+        case 'ang':
+            formattedDate = `${convertToRomanNumerals(day)} ${monthName} ${convertToRomanNumerals(year)}`; break;
         case 'ar':
             formattedDate = `${convertToArabicNumerals(day)} ${monthName} ${convertToArabicNumerals(year)}`; break;
         case 'br':
