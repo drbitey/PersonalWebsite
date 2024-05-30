938675c0df2734fe67b1035ba8831f3d0aa9311d
drbitey <drbitey@gmail.com>
2024-05-30 11:07:21 -0500

    Delete archive/lastmodifieddate.js

--- archive/lastmodifieddate.js
+++
@@ -1,5 +0,0 @@
-function getLastModified() {
-  var text = new Date(document.lastModified);
-  text = text.toISOString().split('T')[0];
-  document.getElementById("lastModifiedDate").innerHTML = "Last update: " + text;
-}
