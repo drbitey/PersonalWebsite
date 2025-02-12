540a920ed99d69c6cc75683e9713226059abf05e
drbitey <drbitey@gmail.com>
2025-02-12 14:52:07 -0600

    Update sotdLanguageSelect.js

--- sotd/sotdLanguageSelect.js
+++ sotd/sotdLanguageSelect.js
@@ -1,5 +1,34 @@
+const languages = [
+	{ iso: 'af', name: 'Afrikaans' },
+	{ iso: 'de', name: 'Deutsch' },
+	{ iso: 'en', name: 'English' },
+	{ iso: 'it', name: 'Italiano' }
+	//{ iso: 'st', name: 'Sesotho' },
+	//{ iso: 'tn-BW', name: 'Setswana' }
+] // Currently a little redundant. localizations.json does exist and I do use it but it seems a little inefficient. This stores languages for use in the dropdown.
+const dropdown = document.getElementById('language-dropdown');
+
+function getCurrentLanguage() {
+	const path = window.location.pathname;
+	const parts = path.split('/');
+	const languageCode = parts[parts.length - 1];
+	return languageCode || 'en';
+}
+
+function prepDropdown() {
+	currentLanguage = getCurrentLanguage();
+	for (let i = 0; i < languages.length; i++) {
+		const option = document.createElement('option');
+		option.value = languages[i].iso;
+		option.textContent = languages[i].name;
+		if (languages[i].iso === currentLanguage) {
+			option.selected = true;
+		}
+		dropdown.appendChild(option);
+	}
+}
+
 function toggleLanguageDropdown() {
-	const dropdown = document.getElementById('language-dropdown');
 	dropdown.style.display = (dropdown.style.display === 'block') ? 'none' : 'block';
 }
 
