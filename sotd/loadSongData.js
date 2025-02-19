51ee667e94c082517b7471c4b969a2d222931b67
drbitey <drbitey@gmail.com>
2025-02-19 10:43:22 -0600

    Update loadSongData.js
    Removed roman numerals function because it was unused

--- sotd/loadSongData.js
+++ sotd/loadSongData.js
@@ -1,32 +1,3 @@
-function convertToRomanNumerals(num) {
-  if (num <= 0 || num > 3999) {
-    return "Too high";
-  }
-  const romanNumerals = [
-    { value: 1000, numeral: "M" },
-    { value: 900, numeral: "CM" },
-    { value: 500, numeral: "D" },
-    { value: 400, numeral: "CD" },
-    { value: 100, numeral: "C" },
-    { value: 90, numeral: "XC" },
-    { value: 50, numeral: "L" },
-    { value: 40, numeral: "XL" },
-    { value: 10, numeral: "X" },
-    { value: 9, numeral: "IX" },
-    { value: 5, numeral: "V" },
-    { value: 4, numeral: "IV" },
-    { value: 1, numeral: "I" }
-  ];
-  let result = "";
-  for (let i = 0; i < romanNumerals.length; i++) {
-    while (num >= romanNumerals[i].value) {
-      result += romanNumerals[i].numeral;
-      num -= romanNumerals[i].value;
-    }
-  }
-  return result;
-}
-
 function formatDateFromISO(isoDate, language) {
     const options = {
         year: "numeric",
